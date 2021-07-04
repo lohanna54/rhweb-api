ada49f249d60ae83cc1fa8f2f14523080ef619d8
lohannalucia <lohannalucia55@gmail.com>
2021-07-04 13:42:33 -0300

    [FIX] Access-Control-Allow-Methods

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ require('dotenv').config({
 
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*")
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
     app.use(cors());
     next();
 });
