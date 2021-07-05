e4510cd3eac2fb012c2d118cd9dd11329ee436cc
lohannalucia <lohannalucia55@gmail.com>
2021-07-05 19:25:31 -0300

    Validação de campos obrigatórios

--- controllers/FuncionarioController.js
+++ controllers/FuncionarioController.js
@@ -6,7 +6,7 @@ const FUNCSITUACAO_ATIVO = 1;
 const FUNCSITUACAO_INATIVO = 2;
 
 class FuncionarioController {
-    async getFuncAtivos(req,res) {
+async getFuncAtivos(req,res) {
         try {
             const funcionarios = await Funcionario.findAll({
                 include: [{ 
@@ -136,85 +136,108 @@ async getByNome (req, res) {
     }
 }
 async create(req,res) {
-    try {
-            
-        let funcSituacaoRes = await Situacao.findByPk(req.body.situacaoId);       
-        if (!funcSituacaoRes) {
-            res.status(400).json({erro: 'Situação não encontrada'});
-        }
-        const regexCpf = /(\.|-)/gm;
-        let cpfFormated = req.body.cpf.replace(regexCpf, "");
-
-        let funcCPF = await Funcionario.findOne({ where: { cpf: cpfFormated } });
-        if(funcCPF){
-            res.status(400).json({erro: 'CPF já cadastrado'});
-        }else {
-            let AVATAR_PICTURE = 'https://i.imgur.com/uOLw2Yo.png';
-            
-            let funcionario = {
-                nome: req.body.nome,
-                cpf: cpfFormated || req.body.cpf,
-                dataNascimento: req.body.dataNascimento,
-                estadoCivil: req.body.estadoCivil,
-                sexo: req.body.sexo,
-                email: req.body.email,
-                graduacao: req.body.graduacao,
-                cep: req.body.cep,
-                numero: req.body.numero,
-                complemento: req.body.complemento,
-                logradouro: req.body.logradouro,
-                cidade: req.body.cidade,
-                bairro: req.body.bairro,
-                celular: req.body.celular,
-                fixo: req.body.fixo,
-                emergencia: req.body.emergencia,
-                nomeReferencia: req.body.nomeReferencia,
-                parentesco: req.body.parentesco,
-                deficiencia: req.body.deficiencia,
-                dtAdm: req.body.dtAdm,
-                cargo: req.body.cargo,
-                ferias: req.body.ferias,
-                salario: req.body.salario,
-                foto: req.body.foto || AVATAR_PICTURE,
-                situacaoId: Number(req.body.situacaoId)
+    function validateRequiredFields(receivedBody){
+        let bodyProperties = {
+            'valid': true,
+            'missingFields': []
+        } 
+        const requiredFields = ['nome','cpf','dataNascimento','estadoCivil','sexo','email','cep','numero','celular','cargo','isAdmin','dtAdm','salario','situacaoId'];
+        requiredFields.forEach(field => {
+            if(!(field in receivedBody)){
+                bodyProperties.valid = false,
+                bodyProperties.missingFields.push(field)
+            }else if(receivedBody[field] == null || receivedBody[field] == "" || receivedBody[field] == undefined){
+                bodyProperties.valid = false,
+                bodyProperties.missingFields.push(field)
             }
-            
-            const funcResponse = await Funcionario.create(funcionario);
-            const loginFuncionario = await createLogin(funcResponse);
-            const funcObj = await Funcionario.update(
-                { loginId: loginFuncionario.id },
-                { where: { id: funcResponse.id } });
-                
-                res.status(201).json({mensagem:`Funcionario ${funcResponse.id} criado com sucesso`});
-                
-                async function createLogin(){
-                    let usuarioObj = funcResponse.nome.split(' ');
-                    let userLogin = usuarioObj[0]+'.'+usuarioObj[usuarioObj.length-1];
-                    userLogin = userLogin.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase();
-                    
-                    let encodedPass = CryptoJS.AES.encrypt(funcResponse.cpf, process.env.ACCESS_SECRET).toString();
-                    
-                    let funcionarioLogin = {
-                        ativo: true,
-                        funcionarioId: funcResponse.id,
-                        usuario: userLogin,
-                        senha: encodedPass,
-                        isAdmin: req.body.isAdmin || false,
-                        token: null
-                    }
-                    console.log(funcionarioLogin)
-                    const loginReponse = await Login.create(funcionarioLogin);
-                    
-                    return loginReponse;
+        });
+        return bodyProperties;
+    }
+    try {
+        let reqObj = req.body;
+        let bodyProperties = await validateRequiredFields(reqObj);
+
+        if(!bodyProperties.valid){
+            res.status(400).json({error: `required fields missing: ${bodyProperties.missingFields}`});
+        }else{
+            let funcSituacaoRes = await Situacao.findByPk(req.body.situacaoId);       
+            if (!funcSituacaoRes) {
+                res.status(400).json({erro: 'Situação não encontrada'});
+            }
+            const regexCpf = /(\.|-)/gm;
+            let cpfFormated = req.body.cpf.replace(regexCpf, "");
+
+            let funcCPF = await Funcionario.findOne({ where: { cpf: cpfFormated } });
+            if(funcCPF){
+                res.status(400).json({erro: 'CPF já cadastrado'});
+            }else {
+                let AVATAR_PICTURE = 'https://i.imgur.com/uOLw2Yo.png';
+
+                let funcionario = {
+                    nome: req.body.nome,
+                    cpf: cpfFormated || req.body.cpf,
+                    dataNascimento: req.body.dataNascimento,
+                    estadoCivil: req.body.estadoCivil,
+                    sexo: req.body.sexo,
+                    email: req.body.email,
+                    graduacao: req.body.graduacao,
+                    cep: req.body.cep,
+                    numero: req.body.numero,
+                    complemento: req.body.complemento,
+                    logradouro: req.body.logradouro,
+                    cidade: req.body.cidade,
+                    bairro: req.body.bairro,
+                    celular: req.body.celular,
+                    fixo: req.body.fixo,
+                    emergencia: req.body.emergencia,
+                    nomeReferencia: req.body.nomeReferencia,
+                    parentesco: req.body.parentesco,
+                    deficiencia: req.body.deficiencia,
+                    dtAdm: req.body.dtAdm,
+                    cargo: req.body.cargo,
+                    ferias: req.body.ferias,
+                    salario: req.body.salario,
+                    foto: req.body.foto || AVATAR_PICTURE,
+                    situacaoId: Number(req.body.situacaoId)
                 }
-                
-            }  
-        }
+
+                const funcResponse = await Funcionario.create(funcionario);
+                const loginFuncionario = await createLogin(funcResponse);
+                const funcObj = await Funcionario.update(
+                    { loginId: loginFuncionario.id },
+                    { where: { id: funcResponse.id } });
+
+                    res.status(201).json({mensagem:`Funcionario ${funcResponse.id} criado com sucesso`});
+
+                    async function createLogin(){
+                        let usuarioObj = funcResponse.nome.split(' ');
+                        let userLogin = usuarioObj[0]+'.'+usuarioObj[usuarioObj.length-1];
+                        userLogin = userLogin.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase();
+
+                        let encodedPass = CryptoJS.AES.encrypt(funcResponse.cpf, process.env.ACCESS_SECRET).toString();
+
+                        let funcionarioLogin = {
+                            ativo: true,
+                            funcionarioId: funcResponse.id,
+                            usuario: userLogin,
+                            senha: encodedPass,
+                            isAdmin: req.body.isAdmin || false,
+                            token: null
+                        }
+                        console.log(funcionarioLogin)
+                        const loginReponse = await Login.create(funcionarioLogin);
+
+                        return loginReponse;
+                    }
+
+                }  
+            }
+    }
         catch(err) {
             res.status(400).json({error: err.message})
         }
-    }
-    async update(req, res){
+}
+async update(req, res){
         try{
             const funcionario = await Funcionario.findByPk(req.params.id);
             if(funcionario){
@@ -269,8 +292,8 @@ async create(req,res) {
         }catch(e){
             return res.status(400).json({error: e.message});
         }
-    }
-    async desativar(req, res){
+}
+async desativar(req, res){
         try{
             const funcionario = await Funcionario.findByPk(req.params.id);
             if(funcionario){
@@ -283,8 +306,8 @@ async create(req,res) {
         }catch(e){
             return res.status(400).json({error: e.message});
         }
-    }
-    async reativar(req, res){
+}
+async reativar(req, res){
         try{
             const funcionario = await Funcionario.findByPk(req.params.id);
             if(funcionario){
@@ -297,7 +320,10 @@ async create(req,res) {
         }catch(e){
             return res.status(400).json({error: e.message});
         }
-    }
+}
+
+
+
 }
 
 module.exports = new FuncionarioController();
